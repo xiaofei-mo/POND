d1e7ef6bdc152da06b4d771b6d050fd8c16781c9
Mark Lindsay <mark@nervous.org>
2016-06-09 21:48:36 +0000

    a little cleaning up

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: forever start index.js
-web-watch: nodemon --ignore src/ --ignore public/ index.js
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
+// nodemon --ignore src/ --ignore public/ index.js
+
 require('babel-core/register');
 require("./server.js");
