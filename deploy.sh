6d6063c1e0c9cafcc985dce4a4d7947f3f038f1f
Mark Lindsay <mark@nervous.org>
2017-04-29 18:05:14 +0000

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,9 @@
 # along with mysteriousobjectsatnoon.  If not, see 
 # <http://www.gnu.org/licenses/>.
 
+# Ensure `jq` is installed.
+sudo apt-get install jq > /dev/null
+
 # Log in to ECR.
 eval `aws ecr get-login --region us-west-2 --profile mysteriousobjectsatnoon`
 
@@ -25,3 +28,22 @@ eval `aws ecr get-login --region us-west-2 --profile mysteriousobjectsatnoon`
 docker build -t mysteriousobjectsatnoon .
 docker tag mysteriousobjectsatnoon:latest 639407601096.dkr.ecr.us-west-2.amazonaws.com/mysteriousobjectsatnoon:latest
 docker push 639407601096.dkr.ecr.us-west-2.amazonaws.com/mysteriousobjectsatnoon:latest
+
+# Stop the existing task.
+EXISTING_TASK_ARN=`aws ecs list-tasks --region us-west-2 --cluster production-cluster --service-name production-service | jq -r '.taskArns[0]'`
+aws ecs stop-task --region us-west-2 --cluster production-cluster --task "$EXISTING_TASK_ARN" > /dev/null
+
+# Get the existing task definition JSON for the production-task-definition
+# family.
+aws ecs describe-task-definition --region us-west-2 --task-definition production-task-definition | \
+# aws ecs register-task-definition is particular about what keys are present.
+jq '.taskDefinition | del(.requiresAttributes, .revision, .status, .taskDefinitionArn)' > production-task-definition.json
+
+# Register a new task definition version.
+NEW_TASK_DEFINITION_ARN=`aws ecs register-task-definition --region us-west-2 --family production-task-definition --cli-input-json file://production-task-definition.json | jq -r '.taskDefinition.taskDefinitionArn'`
+
+# Delete the task definition JSON file.
+rm production-task-definition.json
+
+# Update production-service to use the new task definition ersion.
+aws ecs update-service --region us-west-2 --cluster production-cluster --service production-service --task-definition "$NEW_TASK_DEFINITION_ARN" > /dev/null
